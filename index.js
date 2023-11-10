855745fc2ec308c6b5611bea2ebf016c99af9aad
SemtexWilli07 <128592855+SemtexWilli07@users.noreply.github.com>
2023-11-10 09:35:15 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function showDropdown() {
+    document.querySelector('.dropdown-content').style.display = 'block';
+}
+
+function hideDropdown() {
+    document.querySelector('.dropdown-content').style.display = 'none';
+}
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
